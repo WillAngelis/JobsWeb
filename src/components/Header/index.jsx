c89f2eac39a8f28f0a49de4a45eb164d25324e4a
William Angelis <willangelis@gmail.com>
2022-03-21 02:03:47 -0300

    feat: :sparkles: Criando Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>
+          Github
+          <span> Jobs</span>
+        </h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
